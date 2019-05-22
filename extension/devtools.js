3ef26c633e9d601295bddfd70de203d3bd06bf1d
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-05-22 12:19:37 +0900

    Rename main.* to sidebar-pane.*

--- extension/devtools.js
+++ extension/devtools.js
@@ -2,5 +2,5 @@
 
 (async function install() {
   const pane = await browser.devtools.panels.elements.createSidebarPane("Compatibility");
-  pane.setPage("main.html");
+  pane.setPage("sidebar-pane.html");
 })();
