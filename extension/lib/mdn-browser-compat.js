9f45b48b933a8eb04099453644c3fdea1248f3a5
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-05-29 13:30:33 +0900

    Flatten CSS properties that has prefix

--- extension/lib/mdn-browser-compat.js
+++ extension/lib/mdn-browser-compat.js
@@ -18,6 +18,7 @@ class MDNBrowserCompat {
    */
   constructor(mdnCompatData) {
     this.mdnCompatData = mdnCompatData;
+    this._flattenProperties(this.mdnCompatData.css.properties);
   }
 
   getBrowsers() {
@@ -114,5 +115,81 @@ class MDNBrowserCompat {
       return 0;
     }
     return version === false ? Number.MAX_VALUE : parseFloat(version);
+  }
+
+  _flattenProperties(propertyMap) {
+    const aliases = [];
+    for (let property in propertyMap) {
+      const compatData = propertyMap[property]
+      this._flattenProperty(aliases, property, compatData);
+    }
+
+    for (const { alias, context, runtime,
+                 version_added, version_removed, flags } of aliases) {
+
+      if (!propertyMap[alias]) {
+        propertyMap[alias] = {}
+      }
+
+      let compatData;
+      if (context) {
+        if (!propertyMap[alias][context]) {
+          propertyMap[alias][context] = {};
+        }
+        compatData = propertyMap[alias][context];
+      } else {
+        if (!propertyMap[alias]) {
+          propertyMap[alias] = {};
+        }
+        compatData = propertyMap[alias];
+      }
+
+      if (!compatData.__compat) {
+        compatData.__compat = {};
+      }
+
+      if (!compatData.__compat.support) {
+        compatData.__compat.support = {};
+      }
+
+      const support = compatData.__compat.support;
+      if (!support[runtime]) {
+        support[runtime] = [];
+      } else if (!Array.isArray(support[runtime])) {
+        support[runtime] = [support[runtime]];
+      }
+
+      const supportList = support[runtime];
+      supportList.push({
+        version_added, version_removed, flags,
+      });
+    }
+  }
+
+  _flattenProperty(aliases, property, compatData, context) {
+    if (compatData.__compat) {
+      for (let runtime in compatData.__compat.support) {
+        const supportStates = compatData.__compat.support[runtime] || [];
+        for (const { alternative_name, prefix, version_added, version_removed, flags }
+          of Array.isArray(supportStates) ? supportStates : [supportStates]) {
+          if (!prefix && ! alternative_name) {
+            continue;
+          }
+
+          const alias = alternative_name || prefix + property;
+          aliases.push({
+            alias, context, runtime, version_added, version_removed, flags,
+          });
+        }
+      }
+
+      return;
+    }
+
+    for (let field in compatData) {
+      if (field.endsWith("_context")) {
+        this._flattenProperty(aliases, property, compatData[field], field);
+      }
+    }
   }
 }
